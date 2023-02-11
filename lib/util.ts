009da7cad5666b4d91f23e4ad178bbf4e64bd977
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-02-11 16:48:24 +0000

    Use ec2

---
+++ lib/util.ts
@@ -0,0 +1,50 @@
+import { Octokit } from "octokit";
+import * as sodium from "@devtomio/sodium";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  override: true,
+});
+
+const { GITHUB_TOKEN } = process.env;
+
+// github client
+const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+const getPublicKey = async () => {
+  const pubKey = await octokit.request(
+    "GET /repos/{owner}/{repo}/actions/secrets/public-key",
+    {
+      owner: "Hacksore",
+      repo: "Hacksore",
+    }
+  );
+
+  return pubKey.data;
+};
+
+// helper to set secrets
+export const setRepoSecret = async (secretName: string, secretValue: string) => {
+  const pubKey = await getPublicKey();
+
+  // Convert the message and key to Uint8Array's (Buffer implements that interface)
+  const messageBytes = Buffer.from(secretValue);
+  const keyBytes = Buffer.from(pubKey.key, 'base64');
+
+  // Encrypt using LibSodium.
+  const encryptedBytes = sodium.crypto_box_seal(messageBytes, keyBytes);
+
+  // Base64 the encrypted secret
+  const encryptedEncryptionKey = Buffer.from(encryptedBytes).toString('base64');
+
+  await octokit.request(
+    "PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}",
+    {
+      owner: "Hacksore",
+      repo: "Hacksore",
+      secret_name: secretName,
+      encrypted_value: encryptedEncryptionKey,
+      key_id: pubKey.key_id
+    }
+  );
+};
