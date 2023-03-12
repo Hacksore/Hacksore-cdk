0c68980f58aac4c037528d2b1d705e74c48f3ddc
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-11 18:19:44 -0600

    ok fr fr lightsail this time, source: TRUST ME BRO

--- lib/index.ts
+++ lib/index.ts
@@ -1,23 +1,15 @@
 import { Construct } from "constructs";
-import { aws_ec2 as ec2, aws_iam as iam } from "aws-cdk-lib";
+import { aws_lightsail as lightsail } from "aws-cdk-lib";
 import { readFileSync } from "fs";
 import * as cdk from "aws-cdk-lib";
-
-import * as dotenv from "dotenv";
-
-dotenv.config({
-  override: true,
-});
-
-const { DISCORD_TOKEN, FIREBASE_SA_BASE64, GH_TOKEN } = process.env;
-
-import { setRepoSecret } from "./util";
+import "dotenv/config";
 
 export interface CdkProps {
   // Define construct properties here
 }
 
-if (!GH_TOKEN) throw new Error("GH_TOKEN env var must be set");
+const { DISCORD_TOKEN, FIREBASE_SA_BASE64 } = process.env;
+
 if (!DISCORD_TOKEN) throw new Error("DISCORD_TOKEN env var must be set");
 if (!FIREBASE_SA_BASE64)
   throw new Error("FIREBASE_SA_BASE64 env var must be set");
@@ -35,71 +27,21 @@ const createUserDataScript = () => {
 };
 
 export class DiscordPresenceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: CdkProps = {}) {
+  constructor(scope: Construct, id: string, _props: CdkProps = {}) {
     super(scope, id);
 
-    this.createBotStack();
-  }
-
-  async createBotStack() {
-    // default VPC
-    const vpc = new ec2.Vpc(this, "DiscordPresenceVPC", {
-      subnetConfiguration: [
-        {
-          cidrMask: 26,
-          name: "Public",
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-      ],
+    const lightsailAZ = "us-east-1a";
+    const vmInstance = new lightsail.CfnInstance(this, "DiscordPresence", {
+      blueprintId: "amazon_linux_2",
+      bundleId: "nano_2_0",
+      instanceName: "discord-presence",
+      availabilityZone: lightsailAZ,
+      userData: createUserDataScript(),
     });
-
-    // crreate a role
-    const role = new iam.Role(this, "DiscordPresenceKeyPairRole", {
-      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
-    });
-
-    // security group
-    const securityGroup = new ec2.SecurityGroup(this, "DiscordPresenceSG", {
-      vpc,
-      allowAllOutbound: true,
-    });
-
-    // add ssh port
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      "Allows SSH access from Internet"
-    );
-
-    // Finally lets provision our ec2 instance
-    const vmInstance = new ec2.Instance(this, "DiscordPresence", {
-      vpc,
-      role: role,
-      securityGroup,
-      instanceName: "DiscordPresence",
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO
-      ),
-      machineImage: ec2.MachineImage.latestAmazonLinux({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      keyName: "DiscordPresenceKeyPair",
-    });
-
-    // add the init script
-    vmInstance.addUserData(createUserDataScript());
 
     // store ip address
     new cdk.CfnOutput(this, "ipAddress", {
-      value: vmInstance.instancePublicIp,
+      value: vmInstance.attrPublicIpAddress,
     });
-
-    // set IP as a action secret
-    // console.log("Setting IP", vmInstance.instancePublicIp);
-    // await setRepoSecret("BOT_HOST_IP", vmInstance.instancePublicIp);
-
-    // TODO: seems like limitation of the CDK/CF you cant get priv key
-    // await setRepoSecret("BOT_HOST_PRIVATE_KEY", keyPair.);
   }
 }
