7ba3f4301574abafcb6401eb16f5b231abf610f0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-11 19:34:50 -0500

    Try warp?

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,9 +18,25 @@ DISCORD_TOKEN={DISCORD_TOKEN}
 GITHUB_ACCESS_TOKEN={GITHUB_ACCESS_TOKEN}
 ' > /home/ec2-user/.env
 
+# setup warp?
+echo "WARP mode: client warp+doh."
+
+# install cloudflare warp
+curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | sudo tee /etc/yum.repos.d/cloudflare-warp.repo
+
+# Update repo
+sudo yum update
+sudo yum install cloudflare-warp
+
+sudo warp-cli --accept-tos registration new
+sudo warp-cli --accept-tos mode warp+doh
+
+sudo warp-cli --accept-tos add-excluded-route 0.0.0.0/0
+sudo warp-cli --accept-tos connect
+
 # pull discord image
 docker run -d \
-  --name presence \
-  --env-file=/home/ec2-user/.env \
-  --restart=always \
-  hacksore/presence-bot:latest
+	--name presence \
+	--env-file=/home/ec2-user/.env \
+	--restart=always \
+	hacksore/presence-bot:latest
